0a4029e3d894f0907c53e6cc640c54783792dbf0
Naoya Iijima <meteor5963@gmail.com>
2025-05-24 16:51:17 +0900

    setup🛠️

---
+++ kpt-frontend/app/store/types.ts
@@ -0,0 +1,12 @@
+// 状態
+interface SampleState {
+    count: number;
+}
+  
+  // アクション
+interface SampleActions {
+    increment: () => void;
+    decrement: () => void;
+}
+  
+export type StoreState = SampleState & SampleActions;
