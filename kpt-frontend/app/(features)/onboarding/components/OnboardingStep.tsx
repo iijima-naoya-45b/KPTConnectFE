bc8787f5efa85a67af51c3ea731dd5245403525b
Naoya Iijima <meteor5963@gmail.com>
2025-06-30 00:49:47 +0900

    wip

--- kpt-frontend/app/(features)/onboarding/components/OnboardingStep.tsx
+++ kpt-frontend/app/(features)/onboarding/components/OnboardingStep.tsx
@@ -25,7 +25,8 @@ const OnboardingStep: React.FC<OnboardingStepProps> = ({ step, currentStep, tota
         </div>
         <div className='w-full bg-gray-200 rounded-full h-2'>
           <div
-            className={`bg-indigo-600 h-2 rounded-full transition-all duration-300 w-[${((currentStep + 1) / totalSteps) * 100}%]`}
+            className="bg-indigo-600 h-2 rounded-full transition-all duration-300"
+            style={{ width: `${((currentStep + 1) / totalSteps) * 100}%` }}
           ></div>
         </div>
       </div>
